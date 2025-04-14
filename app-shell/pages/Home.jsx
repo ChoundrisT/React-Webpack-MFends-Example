a619a029918aa4df4d1718d20934306999ff93b5
ChoundrisT <theodoros.choundris@ist.com.gr>
2025-04-14 11:12:54 +0300

    onClick remote component routing

--- app-shell/pages/Home.jsx
+++ app-shell/pages/Home.jsx
@@ -1,12 +1,17 @@
 import React, { Suspense, lazy } from "react";
-import { Link } from "react-router-dom";
-import ErrorBoundary from "../components/ErrorBoundary";
+import { Link, useNavigate } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Lazy load the Button component
+
 const Button = lazy(() => import("app1/Button").catch(() => ({ default: () => <div>Error loading button</div> })));
 
 export default function Home() {
+   const navigate = useNavigate(); 
+
+   const handleButtonClick = () => {
+      navigate("/vans"); 
+   };
+
    return (
       <div className="home-container">
          <h1>You got the travel plans, we got the travel vans.</h1>
@@ -14,7 +19,7 @@ export default function Home() {
          <Link to="vans">Find your van</Link>
          {/* <ErrorBoundary> */}
             <Suspense fallback={<div>Loading...</div>}>
-               <Button>Button from Project App1</Button>
+               <Button onClick={handleButtonClick}>Button from Project App1, also going to /vans</Button>
             </Suspense>
          {/* </ErrorBoundary> */}
       </div>
